56f707c0bb1b19e511f32f44cc5ac5b64bf53464
Jason Harris <lord.jtharris@gmail.com>
2023-10-06 22:13:51 -0500

    Redid the local git repository, made program to convert dollars to coin amounts.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+Console.WriteLine("Please enter the amount you would like to convert to quarters, dimes, nickels, and pennies.");
+var input = Console.ReadLine();
+decimal quarter = 0.25M;
+decimal dime = 0.10M;
+decimal nickel = 0.05M;
+decimal penny = 0.01M;
+string returnString = input + " can be broken down into: ";
+if(!decimal.TryParse(input, out decimal convertedString))
+{
+    Console.WriteLine("You didn't enter a valid decimal.");
+}
+else
+{
+    returnString += decimal.ToInt32((convertedString / quarter)) + " quarters, ";//Divide the input by a quarter
+    decimal remainder = convertedString % quarter;
+    if ((remainder / dime) < 1)
+    {
+        returnString += "0 dimes, ";
+    }
+    else
+    {
+        returnString += decimal.ToInt32(remainder / dime) + " dimes, ";
+        remainder = remainder % dime;
+        Console.WriteLine(remainder);
+    }
+    if((remainder / nickel) < 1)
+    {
+        returnString += "0 nickels, ";
+    }
+    else
+    {
+        returnString += decimal.ToInt32(remainder / nickel) + " nickels, ";
+        remainder = remainder % nickel;
+    }
+    if((remainder / penny) < 1)
+    {
+        returnString += "0 pennies.";
+    }
+    else
+    {
+        returnString += decimal.ToInt32(remainder / penny) + " pennies.";
+    }
+}
+
+Console.WriteLine(returnString);
